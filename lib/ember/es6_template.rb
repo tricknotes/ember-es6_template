876a56643ce469008cf9ea7cf4e4aa2c27f66b32
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-04-01 01:41:46 +0900

    Fix for `.module.es6` with sprockets 3
    It is too strict to content type.

--- lib/ember/es6_template.rb
+++ lib/ember/es6_template.rb
@@ -19,13 +19,8 @@ module Ember
       autoload :ES6Module, 'ember/es6_template/es6module'
 
       def self.setup(env)
-        env.register_mime_type 'text/ecmascript-6', extensions: ['.es6'], charset: :unicode
-        env.register_transformer 'text/ecmascript-6', 'application/javascript', ES6
-        env.register_preprocessor 'text/ecmascript-6', Sprockets::DirectiveProcessor
-
-        env.register_mime_type 'text/ecmascript-6+module', extensions: ['.module.es6'], charset: :unicode
-        env.register_transformer 'text/ecmascript-6+module', 'application/javascript', ES6Module
-        env.register_preprocessor 'text/ecmascript-6+module', Sprockets::DirectiveProcessor
+        env.register_engine '.es6', ES6, mime_type: 'application/javascript'
+        env.register_engine '.module.es6', ES6Module, mime_type: 'application/javascript'
       end
     else
       raise "Unsupported sprockets version: #{Sprockets::VERSION}"
