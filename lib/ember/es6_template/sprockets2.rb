48c356a0293d58fe68f5573d9d57b69204f7cd05
Ryunosuke SATO <tricknotes.rs@gmail.com>
2015-04-05 17:36:54 +0900

    Fix to work without coffee-script

--- lib/ember/es6_template/sprockets2.rb
+++ lib/ember/es6_template/sprockets2.rb
@@ -1,3 +1,9 @@
+begin
+  require 'coffee_script'
+rescue LoadError => e
+  raise e unless e.message == 'cannot load such file -- coffee_script'
+end
+
 module Ember
   module ES6Template
     autoload :ES6, 'ember/es6_template/sprockets2/es6'
@@ -7,7 +13,7 @@ module Ember
     def self.setup(env)
       env.register_engine '.es6', ES6
       env.register_engine '.module', ES6Module
-      env.register_engine '.coffee', CoffeeScript
+      env.register_engine '.coffee', CoffeeScript if defined?(::CoffeeScript)
     end
   end
 end
--- lib/ember/es6_template/sprockets2/es6.rb
+++ lib/ember/es6_template/sprockets2/es6.rb
@@ -1,5 +1,3 @@
-require 'coffee_script'
-
 module Ember
   module ES6Template
     class ES6 < ::Tilt::Template
